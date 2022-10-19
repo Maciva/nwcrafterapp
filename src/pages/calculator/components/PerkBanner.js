34f6a5450b3bc5de48f642e89a4d0b47504c0c44
Mathias Ivanov <m.ivanov@ostfalia.de>
2022-10-19 20:45:24 +0200

    adjusted perkbanner

--- src/pages/calculator/components/PerkBanner.js
+++ src/pages/calculator/components/PerkBanner.js
@@ -1,4 +1,4 @@
-import { Grid, Tooltip } from "@mui/material";
+import { Grid, IconButton, Tooltip } from "@mui/material";
 import React from "react";
 import { useDrag } from "react-dnd";
 import { ItemTypes } from "../../../utils/ItemTypes";
@@ -6,9 +6,10 @@ import PerkIcon from "./PerkIcon";
 import PerkNameDesc from "./PerkNameDesc";
 import PerkLabel from "./PerkLabel";
 import CharmIcon from "./CharmIcon";
+import AddIcon from '@mui/icons-material/Add';
 
 function PerkBanner(props) {
-  const { perk, draggable, disabled, itemClass, gs } = props;
+  const { perk, draggable, disabled, itemClass, gs, onSelect } = props;
 
   const [{ isDragging }, drag] = useDrag(() => ({
     type: ItemTypes.PERK,
@@ -36,24 +37,41 @@ function PerkBanner(props) {
             padding: "1em 2em 1em 0.5em",
             height: "12em",
           }}
-          direction="column"
           justifyContent="space-between"
         >
-          <Grid container item>
-            <Grid container item xs={2} justifyContent="space-between" direction="column">
+          <Grid container item xs={2} justifyContent="space-between" direction="column">
+            <Grid item>
               <PerkIcon imgSrc={imgSrc} />
+            </Grid>
+            <Grid item>
               <CharmIcon charmSrc={charmSrc} perk={perk} isDragging={isDragging} />
             </Grid>
-            <Grid
-              container
-              item
-              xs={10}
-              spacing={1}
-              justifyContent="space-between"
-              direction="column"
-            >
+          </Grid>
+          <Grid
+            container
+            item
+            xs={10}
+            wrap="nowrap"
+            justifyContent="space-between"
+            direction="column"
+          >
+            <Grid item xs={10} >
               <PerkNameDesc perk={perk} gs={gs} itemClass={itemClass} />
-              <PerkLabel perk={perk} />
+            </Grid>
+            <Grid container justifyContent="space-between" item xs={2} >
+              <Grid item>
+                <PerkLabel perk={perk} />
+              </Grid>
+              <Grid item>
+                {
+                  (onSelect &&
+                    <IconButton onClick={() => onSelect(perk)} style={{ padding: 0 }} >
+                      <AddIcon fontSize="large" />
+                    </IconButton>
+                  )
+                }
+
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
